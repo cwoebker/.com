5d5ac5f865df4a9e974d5d6bfb04d3abc30d3748
cwoebker <cwoebker@gmail.com>
2012-09-06 16:16:25 -0400

    added fable for easy deploy etc.

---
+++ fabfile.py
@@ -0,0 +1,25 @@
+from fabric.api import *
+from fabric.contrib.console import confirm
+
+
+def test():
+    local("jekyll")
+
+
+def commit():
+    local("git add -A")
+    local("git commit")
+
+
+def prepare_deploy():
+    test()
+    commit()
+
+
+def push():
+    local("git push origin master")
+
+
+def deploy():
+    if confirm("Ready. Deploy?"):
+        local("git push heroku master")
